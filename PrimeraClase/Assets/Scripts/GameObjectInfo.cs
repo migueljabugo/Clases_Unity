164101d668f3dd9dea9b67df61802cfd9938efeb
Miguel Angel González Hernández <migueljabugo@gmail.com>
2017-11-22 20:30:35 +0100

    Comenzando con scripts

---
+++ PrimeraClase/Assets/Scripts/GameObjectInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectInfo : MonoBehaviour {
+    public float duracion = 5;
+
+
+    void Start () {
+        string goName = gameObject.name;
+        Debug.Log(goName);
+       
+        Debug.Log("El game object " + goName + " se autodestruira en "+ duracion + " segundos.");
+        Destroy(gameObject.gameObject, duracion);
+        
+	}
+
+    private void OnDestroy()
+    {
+        Debug.Log("Taluego");
+    }
+
+
+}
---
+++ PrimeraClase/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour {
+
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+
+
+    private void Start () {
+        Debug.Log("Start");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("OnDisable");
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("OnDestroy");
+    }
+
+    private void Update () {
+
+    }
+}
