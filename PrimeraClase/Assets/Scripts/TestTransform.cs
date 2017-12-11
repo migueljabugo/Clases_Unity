30bc762b230e0957a483b49b37f38af4a05f09b1
Miguel Angel González Hernández <migueljabugo@gmail.com>
2017-12-11 20:52:06 +0100

    no message

---
+++ PrimeraClase/Assets/Scripts/TestTransform.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTransform : MonoBehaviour {
+
+    public Vector3 global;
+    public Vector3 local;
+
+
+	void Update () {
+        global = transform.position;
+        local = transform.localPosition;
+	}
+}
